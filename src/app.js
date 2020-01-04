1b294fc66e50a19f4a451df32a8e5f1e6871fc0e
Max Zakharzhevskiy <max.za@digitalpersona.com>
2020-01-03 19:17:03 -0800

    Initial server

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.static('./public'));
+
+app.use(morgan('combined'));
+app.use(helmet());
+app.use(cors());
+
+app.get('/youtube', async (req, res) => {
+    res.json(['OK']);
+});
+
+function notFound(req, res, next) {
+    const error = new Error('Not found');
+    next(error);
+}
+
+function errorHandler(error, req, res, next) {
+    res.status(res.statusCode || 500);
+    res.json({
+        message: error.message
+    });
+
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
